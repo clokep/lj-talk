ef189be9762b58851dec68748fd695bc3b861671
clokep <clokep@gmail.com>
2012-01-03 21:51:02 -0500

    Relicense code under MPL 2.0.

--- ljtalk.js
+++ ljtalk.js
@@ -1,38 +1,8 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is LJ Talk.
- *
- * The Initial Developer of the Original Code is
- * Patrick Cloke <clokep@gmail.com>.
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/*
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ */
 
 const {interfaces: Ci, utils: Cu} = Components;
 
@@ -58,7 +28,7 @@ LJTalkAccount.prototype = {
       this._jid = this._parseJID(this.name + "@livejournal.com/instantbird");
     else
       this._jid = this._parseJID(this.name);
-      
+
 
     this._connection = new XMPPSession("livejournal.com", 5222,
                                        "require_tls", this._jid,
@@ -69,7 +39,7 @@ LJTalkAccount.prototype = {
 function ljtalkProtocol() { }
 ljtalkProtocol.prototype = {
   __proto__: GenericProtocolPrototype,
-  
+
   // This should match the second half of the category in the chrome.manifest
   // file (e.g. prpl-ljtalk).
   get normalizedName() "ljtalk",
@@ -81,11 +51,11 @@ ljtalkProtocol.prototype = {
   get iconBaseURI() "chrome://prpl-ljtalk/skin/",
 
   getAccount: function(aImAccount) new LJTalkAccount(this, aImAccount),
-  
+
   options: {
     resource: {get label() _("options.resource"), default: "instantbird"}
   },
-  
+
   // A UUID, you can ask instantbot in #instantbird on moznet to generate one
   // for you (by asking "instantbot: uuid"). This needs to match the component
   // IDs in your chrome.manifest.
